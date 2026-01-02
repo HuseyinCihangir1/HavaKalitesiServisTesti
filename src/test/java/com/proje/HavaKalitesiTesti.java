e10fe23e4eeee9a716d5038ed136ccaa606ffb1b
HuseyinCihangir1 <hyccihangir@gmail.com>
2026-01-02 16:22:20 +0300

    proje son haline getirildi, run tests: 0 hatasi giderildi

--- src/test/java/com/proje/HavaKalitesiTesti.java
+++ src/test/java/com/proje/HavaKalitesiTesti.java
@@ -4,11 +4,10 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Test;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
 
 public class HavaKalitesiTesti extends TestTaban {
 
-    // OpenWeatherMap üzerinden alınan bireysel API anahtarı
+    // OpenWeatherMap uzerinden alinen bireysel api anahtari:
     private final String API_KEY = "8bcc663557e95b0586e37e96b6531c39";
 
     @Test
@@ -21,24 +20,21 @@ public class HavaKalitesiTesti extends TestTaban {
                 .when()
                 .get("https://api.openweathermap.org/data/2.5/air_pollution");
 
-        // Gelen yanıtı konsola yazdır (Neden hata aldığımızı görmek için)
-        response.prettyPrint();
-
-        // KONTROL MANTIĞI:
+        // KONTROL kismi:
         int statusCode = response.getStatusCode();
 
         if (statusCode == 200) {
             // Eğer anahtar aktifse ve 200 dönerse veriyi kontrol et
             response.then().body("list", notNullValue());
-            System.out.println("Test Başarılı: Hava kalitesi verisi alındı.");
+            System.out.println("Test Basarili: Hava kalitesi verisi alindi.");
         } else if (statusCode == 401) {
             // Eğer anahtar henüz aktif değilse 401 döner, bu durumda hata mesajını doğrula
             response.then().body("message", containsString("Invalid API key"));
             System.out.println(
-                    "Test Başarılı: Beklenen 'Yetkisiz Erişim' hatası alındı (API Anahtarı aktivasyon sürecinde).");
+                    "Test Basarili: Beklenen 'Yetkisiz Erişim' hatasi alindi (API Anahtari aktivasyon sürecinde).");
         } else {
-            // Beklenmedik bir durum (404, 500 vb.) olursa testi fail et
-            throw new AssertionError("Beklenmedik durum kodu alındı: " + statusCode);
+            // Beklenmedik bir hata durumu(404, 500 vb.) olursa testi fail et
+            throw new AssertionError("Beklenmedik durum kodu alindi: " + statusCode);
         }
     }
 
